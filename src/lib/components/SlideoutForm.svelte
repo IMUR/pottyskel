58c080874f49eeeda7aaccd2ef36593cac9cc93a
IMUR <rjallen22@gmail.com>
2024-07-16 16:35:09 +0000

    asdasd

--- src/lib/components/SlideoutForm.svelte
+++ src/lib/components/SlideoutForm.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import { potties } from '../utils/stores';
   import { onMount } from 'svelte';
-
+  import { getDrawerStore } from '@skeletonlabs/skeleton';
+  const drawerStore = getDrawerStore();
   interface Suggestion {
     formatted: string;
   }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,9 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	import { initializeStores, Drawer } from '@skeletonlabs/skeleton';
+  initializeStores();
 </script>
 
 <slot />
+
